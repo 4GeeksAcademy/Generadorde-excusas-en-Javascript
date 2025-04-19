b7aaeff067d77f51b39e5db76ab5856dd132c7ca
Sebastian <133646707+Hizuis@users.noreply.github.com>
2025-04-19 12:13:52 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+    let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+    let action = ['ate', 'peed', 'crushed', 'broke'];
+    let what = ['my homework', 'my phone', 'the car'];
+    let when = [
+      'before the class',
+      'when I was sleeping',
+      'while I was exercising',
+      'during my lunch',
+      'while I was praying'
+    ];
+  
+    function generateExcuse() {
+      let whoRandom = who[Math.floor(Math.random() * who.length)];
+      let actionRandom = action[Math.floor(Math.random() * action.length)];
+      let whatRandom = what[Math.floor(Math.random() * what.length)];
+      let whenRandom = when[Math.floor(Math.random() * when.length)];
+  
+      return `${whoRandom} ${actionRandom} ${whatRandom} ${whenRandom}.`;
+    }
+  
+    document.getElementById("excuse").innerHTML = generateExcuse();
+  };
+  
